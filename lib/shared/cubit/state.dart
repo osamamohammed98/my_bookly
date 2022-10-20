a56e9a2524d091ac4a68c50d46dcdc89f52e0b2d
osama <om107547@gmail.com>
2022-10-20 12:43:33 +0300

    add search

--- lib/shared/cubit/state.dart
+++ lib/shared/cubit/state.dart
@@ -2,13 +2,4 @@ abstract class AppState{}
 
 class AppInitState extends AppState{}
 
-class AppChangeBottomNavBarState extends AppState{}
-
-class AppCreateDatabaseState extends AppState {}
-class AppGetDatabaseState extends AppState {}
-class AppGetDatabaseLoadingState extends AppState {}
-class AppInsertDatabaseState extends AppState {}
-class AppUpdateDatabaseState extends AppState {}
-class AppDeleteDatabaseState extends AppState {}
 class AppThemeChangedState extends AppState {}
-class AppChangeBottomSheetState extends AppState {}
